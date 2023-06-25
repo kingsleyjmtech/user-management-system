38e95c7db5c4c0bc8f36f792cee5edb7df24cf0b
Kingsley Moseamedi <kingsleyjmtech@gmail.com>
2023-06-25 20:35:23 +0200

    Updated User fillable

--- user-management-system-app/app/Models/User.php
+++ user-management-system-app/app/Models/User.php
@@ -18,9 +18,16 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'phone',
+        'username',
         'email',
         'password',
+        'active',
+        'terms_accepted',
+        'creator_id',
+        'slug',
     ];
 
     /**
